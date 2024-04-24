0ccd9425539bdf21020a010ec685154d20bc4c9c
Mohammad Imran Hossain <imran.hossain@brainstaion23.com>
2024-04-24 23:52:05 +0600

    Python: 	- Simple age & decade calculator

---
+++ decades.py
@@ -0,0 +1,8 @@
+age  = int(input('What is your age\n'))
+print(age)
+decade = 10
+years = int(age / decade)
+fraction = age % decade
+
+print('Your age in decades is', years, 'and', fraction, 'years.')
+
---
+++ tax.py
@@ -0,0 +1,7 @@
+total = 100
+
+percentage = 10
+
+amount = total + (total * percentage / 100)
+
+print(amount)
