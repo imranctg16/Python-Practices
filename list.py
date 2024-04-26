f05140cdee49489a723192f33a4b017f7a282a7f
Mohammad Imran Hossain <imran.hossain@brainstaion23.com>
2024-04-26 11:18:11 +0600

    Python Pracise: 	- Added List File

---
+++ list.py
@@ -0,0 +1,13 @@
+example = []
+
+for i in range(7):
+    example.append(i)
+
+for element in example:
+    print(element)
+
+
+#delete an element 
+example.remove(3) 
+
+del example[0] #remove using index
