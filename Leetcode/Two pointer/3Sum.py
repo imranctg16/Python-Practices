c559fd39460600fb7fb8d2f1fb5f096fca76a57d
Mohammad Imran Hossain <imran.hossain@brainstaion23.com>
2024-05-14 23:33:50 +0600

    Solution: 	- Updated 3 sum.py

--- Leetcode/Two pointer/3Sum.py
+++ Leetcode/Two pointer/3Sum.py
@@ -32,22 +32,22 @@ class Solution:
         print(nums)
         length = len(nums)
         for index,number in enumerate(nums):
-            if(index + 1  < length) and  nums[index] == nums[index+1]:
-                continue
+            if index > 0 and number == nums[index-1]:
+                 continue
             left = index + 1
             right = length - 1 
             while left < right:
                 sum = nums[index] + nums[left] + nums[right]
-                print(nums[left],nums[right])
-                if(sum == 0):
+                print((nums[index], nums[left],nums[right]))
+                if sum == 0:
                     answer.append([nums[index],nums[left],nums[right]])
-                    left += 1
-                    right-=1
                     # we have to move the cursor so that we dont get the same value 
                     while left < right and nums[left] == nums[left+1]:
                             left+=1
                     while left < right and nums[right] == nums[right-1]:
                             right-=1
+                    left += 1
+                    right-=1
                 elif sum < 0:
                     # we need more bigger number
                     left += 1
