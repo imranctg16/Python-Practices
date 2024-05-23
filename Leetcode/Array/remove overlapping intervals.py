16196b56436742a86e75e57328fc1c00f09f27f8
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-05-24 01:40:40 +0600

    Solution: 	- Non Overlapping Intervals

---
+++ Leetcode/Array/remove overlapping intervals.py
@@ -0,0 +1,24 @@
+class Solution:
+   def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+      intervals.sort(key=lambda x: x[1]) # sort them based on the second value 
+      counter = 0
+      end = intervals[0]
+      for i in range(1,len(intervals)):
+        if intervals[i][0] < end[1]:
+          counter+=1
+        else:
+           end = intervals[i]
+      return counter
+   
+
+solution = Solution()
+nums = [[1,3],[2,6],[8,10],[15,18]]
+nums = [[1,4],[4,5]]
+nums = [[1,2],[2,3],[3,4],[1,3]]
+nums = [[1,2],[1,3],[2,3],[3,4]]
+nums = [[1,2],[1,2],[1,2]]
+nums = [[0,2],[1,3],[2,4],[3,5],[4,6]]
+nums = [[0,2],[1,3],[1,3],[2,4],[3,5],[3,5],[4,6]]
+nums = [[1,100],[11,22],[1,11],[2,12]]
+answer = solution.eraseOverlapIntervals(nums)
+print(answer)
