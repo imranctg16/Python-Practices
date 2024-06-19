a591ae7fee158285a3fb96cf183cfaac2f87f609
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-06-20 02:05:29 +0600

    Solution: 	- Monotonic Array

--- Leetcode/Array/monotonic array.py
+++ Leetcode/Array/monotonic array.py
@@ -1,8 +1,23 @@
 class Solution:
-    def is_monotonic(self,nums):
-        pass
+    def isMonotonic(self,nums):
+       flag = None
+       for i in range(len(nums)):
+           if i == 0:
+               continue
+           if flag is None and  nums[i] < nums[i-1]:
+               flag = 0 
+           elif flag is None and  nums[i] > nums[i-1]:
+               flag = 1
+               
+           if flag == 0 and nums[i] > nums[i-1]:
+               return False
+           if flag == 1 and nums[i] < nums[i-1]:
+               return False
+        
+       return True
 
-
-nums =  [1,3,2]
-result = Solution().is_monotonic(nums)
+# nums =  [1,3,2]
+# nums = [1,2,2,3]
+nums = [1,1,0]
+result = Solution().isMonotonic(nums)
 print(result)
