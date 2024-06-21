c973abb5b947c8ba9ccdc5f2692e22d599c53ce4
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-06-21 23:29:50 +0600

    Solution: 	- Can Place Flower

---
+++ Leetcode/Array/can place flowers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        pass
+
+
+flowerbed = [0,0,1,0,1]
+n = 1
+result = Solution().canPlaceFlowers()
+print(result)
+        
