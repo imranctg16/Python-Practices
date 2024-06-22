95104263d327c9c1afc393e45107bb7db97ce684
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-06-22 17:52:23 +0600

    Solution: 	-Can Place Flower Loop

--- Leetcode/Array/can place flowers.py
+++ Leetcode/Array/can place flowers.py
@@ -1,7 +1,8 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        for i in flowerbed:
+            print(i)
         pass
-
 
 flowerbed = [0,0,1,0,1]
 n = 1
