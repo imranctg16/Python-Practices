41f59402c43c972ebc178f92d339f3d38174fc0c
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-06-23 17:47:08 +0600

    SOlution: 	- Flowerbed

--- Leetcode/Array/can place flowers.py
+++ Leetcode/Array/can place flowers.py
@@ -1,11 +1,19 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
-        for i in flowerbed:
-            print(i)
-        pass
-
+        length = len(flowerbed)
+        i = 0 
+        total = n
+        while(i+1 <= length and total):
+            if flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                total-=1  
+            i+=1    
+        if total:
+            return False
+        else:
+            return True
+        
 flowerbed = [0,0,1,0,1]
 n = 1
-result = Solution().canPlaceFlowers()
+result = Solution().canPlaceFlowers(flowerbed,n)
 print(result)
         
