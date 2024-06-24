ba12cf82a7d25ae2da60e3d74767074e51c0db34
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-06-25 01:02:22 +0600

    Solution: 	- Can Place Flower

--- Leetcode/Array/can place flowers.py
+++ Leetcode/Array/can place flowers.py
@@ -3,14 +3,23 @@ class Solution:
         length = len(flowerbed)
         i = 0 
         total = n
-        for i in range(length):
-            if(flowerbed[i] == 0):
-                pass
-
-        
+        while i < length:
+           if i > 0 and i+1 < length:
+               if flowerbed[i-1] == 0 and flowerbed[i+1] == 0 and flowerbed[i] == 0:
+                   flowerbed[i] = 1
+                   total -= 1
+           elif i == 0 and flowerbed[i] == 0 and i+1 < length and flowerbed[i+1] == 0:
+               flowerbed[i] = 1
+               total -= 1
+           elif i == length-1 and flowerbed[i] == 0 and flowerbed[i-1] == 0:
+               flowerbed[i] = 1
+               total -= 1
+           i += 1
+        return total <= 0
 flowerbed = [1,0,0,0,1]
+flowerbed = [0]
 n = 1
-n = 2
+n = 1
 result = Solution().canPlaceFlowers(flowerbed,n)
 print(result)
         
