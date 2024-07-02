329dff592ece0d2032f3ee67e46139d98f836400
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-07-02 23:29:49 +0600

    Solution: 	- Unique Email Addresses

--- Leetcode/string/unique email address.py
+++ Leetcode/string/unique email address.py
@@ -1,6 +1,14 @@
 class Solution:
     def numUniqueEmails(self, emails: list[str]) -> int:
-        pass
+        answers = []
+        for email in emails:
+            email = email.split('@')
+            email[0] = email[0].replace('.','')
+            email[0] = email[0].split('+')[0]
+            email = email[0] + '@' + email[1]
+            if email not in answers:
+                answers.append(email)
+        return len(answers)
 emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
 result = Solution().numUniqueEmails(emails)
 print(result)
