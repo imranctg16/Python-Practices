24bcebb2fceaad55bd9dcebab16a848007ec3ffb
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-07-05 23:51:29 +0600

    solution: 	- Time needed to buy tickets

--- Leetcode/Array/Time Needed To Buy Tickets.py
+++ Leetcode/Array/Time Needed To Buy Tickets.py
@@ -1,7 +1,12 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
-        pass
+        time = 0
+        for i in range(len(tickets)):
+            pass
+        return time
+
 
 tickets = [2,3,2]
 k = 2 
 result = Solution().timeRequiredToBuy(tickets,k)
+print(result)
