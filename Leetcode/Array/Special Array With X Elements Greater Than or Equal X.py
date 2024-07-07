e66c7063a605bc6d0214c3f613aa27364f71a043
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-07-07 23:49:50 +0600

    Solution: 	- Speical Array With X Elements Greater

--- Leetcode/Array/Special Array With X Elements Greater Than or Equal X.py
+++ Leetcode/Array/Special Array With X Elements Greater Than or Equal X.py
@@ -1,7 +1,15 @@
 class SpecialArray:
-    def findNumber(numbers):
-        pass
+    def specialArray(numbers):
+        for i in range(len(numbers)):
+            counter = 0
+            for number in numbers:
+                if i >= number:
+                    counter += 1
+            if counter == i+1:
+                return i+1
+            
+        return -1
 
 numbers = [3,5]
-result = SpecialArray(numbers)
+result = SpecialArray.specialArray(numbers)
 print(result)
