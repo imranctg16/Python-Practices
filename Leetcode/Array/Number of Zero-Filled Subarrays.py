73ce85a9f45c86d60c87bd0716496fa92b577e01
Mohammad Imran Hossain <imranhossain16.ctg@gmail.com>
2024-07-17 23:57:21 +0600

    Solution: 	- Zero filled sub-array

--- Leetcode/Array/Number of Zero-Filled Subarrays.py
+++ Leetcode/Array/Number of Zero-Filled Subarrays.py
@@ -1,10 +1,17 @@
 class Solution:
     def zeroFilledSubarray(self, nums: list[int]) -> int:
-        count = 0
-        for num in nums:
-            if num == 0:
-                count+=1
-        return count
+       count = 0
+       zero_count = 0
+       for i in range(len(nums)):
+           if nums[i] == 0:
+               zero_count += 1
+           else:
+               # i got a non-zero number 
+               count+= (zero_count * (zero_count +1)) // 2
+               zero_count = 0 
+       count+= (zero_count * (zero_count +1)) // 2 # if the array ends with zeros 
+       return count 
+    
 nums = [0,1,0,3,12]
 result = Solution().zeroFilledSubarray(nums)
 print(result)
